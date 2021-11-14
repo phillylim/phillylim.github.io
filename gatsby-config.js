994a80caea6e3f805012d07ffa6ac3a2b52d6051
Philly <phillytlim@gmail.com>
2021-11-14 13:14:16 -0800

    customizations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,18 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://phillylim.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Philly Lim',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Philly Lim | Student Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `I am a current first year student with a deep passion for problem solving and research.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/phillylim`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `www.linkedin.com/in/phillylim`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
